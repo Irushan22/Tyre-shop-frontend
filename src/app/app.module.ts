4577493cb9c568b495ef99c66f38795765483dbd
Irushan_IT17121866 <it17121866@my.sliit.lk>
2021-01-17 00:26:19 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodycontentComponent } from './bodycontent/bodycontent.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodycontentComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
